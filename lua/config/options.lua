0b9caa977f38a02ca9a5838cd8ba1bc0b9b65c1b
Juan De Luca <juan.f.d.luca@gmail.com>
2024-05-18 16:24:30 +0100

    Tweaks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -64,18 +64,18 @@ vim.cmd('colorscheme monokai-pro-spectrum')
 
 vim.g.blamer_enabled = true
 
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_filetypes = {
-    ["*"] = false,
-    ["javascript"] = true,
-    ["typescript"] = true,
-    ["lua"] = false,
-    ["rust"] = true,
-    ["c"] = true,
-    ["c#"] = true,
-    ["c++"] = true,
-    ["go"] = true,
-    ["python"] = true,
-}
+-- vim.g.copilot_no_tab_map = true
+-- vim.g.copilot_filetypes = {
+--     ["*"] = false,
+--     ["javascript"] = true,
+--     ["typescript"] = true,
+--     ["lua"] = false,
+--     ["rust"] = true,
+--     ["c"] = true,
+--     ["c#"] = true,
+--     ["c++"] = true,
+--     ["go"] = true,
+--     ["python"] = true,
+-- }
 
 vim.o.sessionoptions = "buffers,curdir,tabpages,terminal,localoptions"
