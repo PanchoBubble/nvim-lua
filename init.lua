deebec1afa6afd53bfb334708c51089a701ca184
Juan De Luca <juan.f.d.luca@gmail.com>
2024-05-19 19:30:41 +0100

    Removes comment

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
---[[
--- Setup initial configuration,
---
--- Primarily just download and execute lazy.nvim
---]]
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
