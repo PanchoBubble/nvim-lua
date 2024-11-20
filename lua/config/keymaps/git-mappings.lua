a7323ce444e10fcad6b92ebff4a38e7a7eb0b87b
Juan De Luca <juan.f.d.luca@gmail.com>
2024-11-20 16:13:51 +0000

    updates

--- lua/config/keymaps/git-mappings.lua
+++ lua/config/keymaps/git-mappings.lua
@@ -5,7 +5,8 @@ vim.api.nvim_create_user_command("GitAddAndCommitAll",
         -- Use vim.ui.input to prompt the user
         vim.ui.input({ prompt = "Commit message: " }, function(input)
             if input then
-                local current_branch = vim.fn.systemlist("git branch --show-current")[1]                         -- Get current branch name
+                local current_branch = vim.fn.systemlist("git branch --show-current")
+                    [1] -- Get current branch name
                 if not current_branch then
                     print("Error: Could not determine the current branch")
                     return
@@ -17,11 +18,11 @@ vim.api.nvim_create_user_command("GitAddAndCommitAll",
                 vim.cmd("Git pull --no-edit")
 
                 -- Check if the branch exists on the remote
-                local remote_branch_exists = vim.fn.systemlist("git ls-remote --heads origin " .. current_branch)
+                local remote_branch_exists = vim.fn.systemlist("Git ls-remote --heads origin " .. current_branch)
 
                 if #remote_branch_exists == 0 then
                     -- Branch does not exist on the remote, push with --set-upstream
-                    vim.cmd("Git push --set-upstream origin " .. current_branch)
+                    vim.cmd("!git push --set-upstream origin " .. current_branch)
                 else
                     -- Branch exists, normal push
                     vim.cmd("Git push")
@@ -52,3 +53,24 @@ vim.api.nvim_create_user_command("GitCheckoutNewBranch",
     {})
 
 vim.keymap.set("n", "<leader>gp", "<cmd>Git pull --no-edit<CR>")
+
+vim.keymap.set("n", "<leader>p", "<cmd>GitPushNewBranch<CR>")
+vim.api.nvim_create_user_command("GitPushNewBranch",
+    function()
+        local current_branch = vim.fn.systemlist("git branch --show-current")[1]
+        if not current_branch then
+            print("Error: Could not determine the current branch")
+            return
+        end
+        -- Check if the branch exists on the remote
+        local remote_branch_exists = vim.fn.systemlist("Git ls-remote --heads origin " .. current_branch)
+
+        if #remote_branch_exists == 0 then
+            -- Branch does not exist on the remote, push with --set-upstream
+            vim.cmd("!git push --set-upstream origin " .. current_branch)
+        else
+            -- Branch exists, normal push
+            vim.cmd("Git push")
+        end
+    end,
+    {})
