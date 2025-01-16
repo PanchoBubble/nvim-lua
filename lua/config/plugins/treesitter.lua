87e6e02dd893953fd661fdfc440bd869e88bf7ba
Juan De Luca <juan.f.d.luca@gmail.com>
2025-01-16 07:52:53 -0300

    updates

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -16,6 +16,7 @@ return {
                 ensure_installed = {
                     "bash",
                     "go",
+                    "lua",
                     "html",
                     "javascript",
                     "json",
