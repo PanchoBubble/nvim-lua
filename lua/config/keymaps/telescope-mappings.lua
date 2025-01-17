90af59075aac38872d71328761247d25e2ce0421
Juan De Luca <juan.f.d.luca@gmail.com>
2025-01-17 09:47:37 -0300

    updates fid files

--- lua/config/keymaps/telescope-mappings.lua
+++ lua/config/keymaps/telescope-mappings.lua
@@ -1,17 +1,17 @@
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
-
-vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>")
-
 -- vim.keymap.set("n", "<leader>fw", function()
 --     return "<cmd>Telescope live_grep default_text=" .. vim.fn.expand("<cword>") .. "<CR>"
 -- end, { expr = true })
-
-local function handle_live_grep(default_text)
+--
+local get_telescope_args = function()
     local conf = require('telescope.config').values
     local vimgrep_arguments = vim.deepcopy(conf.vimgrep_arguments)
     table.insert(vimgrep_arguments, '--fixed-strings')
     table.insert(vimgrep_arguments, '--hidden')
-    
+    return vimgrep_arguments
+end
+
+local function handle_live_grep(default_text)
+    local vimgrep_arguments = get_telescope_args()
     require('telescope.builtin').live_grep {
         vimgrep_arguments = vimgrep_arguments,
         prompt_title = '  Find Word',
@@ -23,6 +23,16 @@ vim.keymap.set("n", "<leader>fw", function()
     local default_text = vim.fn.expand("<cword>")
     return handle_live_grep(default_text)
 end)
+
+local hadle_find_files = function()
+    require('telescope.builtin').find_files {
+        find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+        prompt_title = '  Find File',
+    }
+end
+vim.keymap.set("n", "<C-p>", hadle_find_files)
+
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
 
 
 vim.keymap.set('n', 'gr', function() require('telescope.builtin').lsp_references() end, { noremap = true, silent = true })
