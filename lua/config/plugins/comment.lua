009cb2f46f87c7bcbc44c295124e745af1a7548b
JuanOne <juan.f.d.luca@gmail.com>
2025-09-24 11:23:13 +0100

    Add nvim-ts-context-commentstring integration for React/JSX support

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -1,7 +1,44 @@
 return {
     {
         'numToStr/Comment.nvim',
-        opts = {},
+        dependencies = {
+            'JoosepAlviste/nvim-ts-context-commentstring',
+        },
+        config = function()
+            require('Comment').setup({
+                -- Enable treesitter integration for context-aware commenting
+                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+                
+                -- Keymaps (defaults shown for reference)
+                toggler = {
+                    line = 'gcc',  -- Line-comment toggle
+                    block = 'gbc', -- Block-comment toggle
+                },
+                opleader = {
+                    line = 'gc',   -- Line-comment operator
+                    block = 'gb',  -- Block-comment operator
+                },
+                extra = {
+                    above = 'gcO',  -- Comment above
+                    below = 'gco',  -- Comment below  
+                    eol = 'gcA',    -- Comment end of line
+                },
+                
+                mappings = {
+                    basic = true,
+                    extra = true,
+                },
+                
+                sticky = true,  -- Keep cursor position
+                ignore = '^$',  -- Ignore empty lines
+            })
+        end,
         lazy = false,
+    },
+    {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        opts = {
+            enable_autocmd = false, -- Disable since using Comment.nvim integration
+        },
     },
 }
