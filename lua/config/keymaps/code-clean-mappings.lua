57ed5fb475c8c056f579234d7226e83fb9bd3681
JuanOne <juan.f.d.luca@gmail.com>
2025-10-28 09:33:34 +0000

    Eliminate formatter cascade and conflicts in prettify function

--- lua/config/keymaps/code-clean-mappings.lua
+++ lua/config/keymaps/code-clean-mappings.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<leader>qf", "<cmd>lua vim.lsp.buf.code_action()<CR>")
 local function prettify()
   local filetype = vim.bo.filetype
   
-  -- Use conform for supported file types with LSP fallback
+  -- Use conform.nvim exclusively for web languages, no LSP fallback to prevent conflicts
   local conform_files = { 
     "typescript", "javascript", "typescriptreact", "javascriptreact", 
     "scss", "css", "html", "json", "yaml", "markdown" 
@@ -11,14 +11,14 @@ local function prettify()
 
   if vim.tbl_contains(conform_files, filetype) then
     require("conform").format({
-      lsp_fallback = true,
-      async = false,
-      timeout_ms = 1000,
+      async = true,
+      timeout_ms = 2000,
+      lsp_fallback = false,  -- Disable lsp_fallback to prevent conflicts
     })
     return
   end
 
-  -- For other file types, try LSP formatting first
+  -- For other file types, use LSP formatting only
   local clients = vim.lsp.get_clients({ bufnr = 0 })
   local has_formatter = false
   
@@ -30,9 +30,7 @@ local function prettify()
   end
 
   if has_formatter then
-    vim.lsp.buf.format({ async = false, timeout_ms = 1000 })
-  elseif vim.fn.exists(":EslintFixAll") > 0 and (filetype == "typescript" or filetype == "javascript" or filetype == "typescriptreact" or filetype == "javascriptreact") then
-    vim.cmd("EslintFixAll")
+    vim.lsp.buf.format({ async = true, timeout_ms = 2000 })
   else
     vim.notify("No formatter available for " .. filetype, vim.log.levels.WARN)
   end
